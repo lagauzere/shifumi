304c62480fb8e05d732355607e64b4e57aa2e9bd
hugo <hugo.lagauzere@etu.unicaen.fr>
2023-09-06 12:41:43 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,79 @@
+let tab=["pierre","feuille","ciseaux"];
+
+
+let Bpierre = document.getElementById('pierre');
+let Bfeuille = document.getElementById('feuille');
+let Bciseaux = document.getElementById('ciseaux');
+
+function bot(){
+    return Math.floor(Math.random() * 3);
+}
+
+let joueur=document.getElementById('joueur');
+let robot=document.getElementById('robot');
+let resultat=document.getElementById('resultat');
+
+
+Bpierre.addEventListener('click',()=>{
+    // alert("pierre");
+   
+    let val=bot();
+    joueur.textContent="joueur : "+tab[0];
+    robot.textContent="robot : "+tab[val];
+
+    if (val==2){
+        resultat.textContent="gagnant";
+    }
+    else if(val==0){
+        resultat.textContent="execo";
+    }
+    else {
+        resultat.textContent="perdant";
+    }
+
+    
+});
+
+
+
+
+Bfeuille.addEventListener('click',()=>{
+    // alert("feuille");
+    let val=bot();
+
+    joueur.textContent="joueur : "+tab[1];
+    robot.textContent="robot : "+tab[val];
+
+    if (val==0){
+        resultat.textContent="gagnant";
+    }
+    else if(val==1){
+        resultat.textContent="execo";
+    }
+    else  {
+        resultat.textContent="perdant";
+    }
+
+    
+});
+
+
+Bciseaux.addEventListener('click',()=>{
+    // alert("ciseaux");
+    let val=bot();
+    joueur.textContent="joueur : "+tab[2];
+    robot.textContent="robot : "+tab[val];
+
+    if (val==1){
+        resultat.textContent="gagnant";
+    }
+    else if(val==2){
+        resultat.textContent="execo";
+    }
+    else  {
+        resultat.textContent="perdant";
+    }
+
+    
+});
+
